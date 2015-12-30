62665c31781d549a648d39ef3092f797abdb0b7f
Eric Evans <eevans@wikimedia.org>
2015-12-30 12:16:35 -0600

    cleanups for JSON-based REST resources

--- agent/src/main/java/org/wikimedia/caizen/agent/Runner.java
+++ agent/src/main/java/org/wikimedia/caizen/agent/Runner.java
@@ -1,26 +1,37 @@
 package org.wikimedia.caizen.agent;
 
-import static spark.Spark.get;
-
 import java.io.IOException;
 
 import org.wikimedia.cassandra.jmx.Probe;
 
+import spark.Route;
+import spark.Spark;
+
 public class Runner {
 
-    public static void main(String[] args) throws IOException {
+    private static Route wrapContentType(Route route) {
+        return (request, response) -> {
+            response.type("application/json; charset=utf-8");
+            return route.handle(request, response);
+        };
+    }
+
+    private static void get(String path, Route route) {
+        Spark.get(path, "application/json", wrapContentType(route), new JsonTransformer());
+    }
+
+    public static void main(String... args) throws IOException {
 
         if (args.length != 2) {
             System.err.println("Exactly two arguments required (host and port)");
             System.exit(1);
         }
 
-        Probe probe = new Probe(args[0], Integer.parseInt(args[1]));
+        final Probe probe = new Probe(args[0], Integer.parseInt(args[1]));
 
-        get("/v1/info", (request, response) -> {
-            response.type("application/json; charset=utf-8");
-            return probe.getNode();
-        } , new JsonTransformer());
+        get("/v1/info", (req, res) -> probe.getNode());
+        get("/v1/streams", (req, res) -> probe.getStreams());
+        get("/v1/metrics", (req, res) -> probe.getMetrics());
 
     }
 
